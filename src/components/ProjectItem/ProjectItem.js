beb44eeeeadc614729a94b556447ed7f8d088174
Weiyi Zhu <zhuwy99@gmail.com>
2021-04-01 22:20:09 -0700

    design project item layout with 9 PM project

--- src/components/ProjectItem/ProjectItem.js
+++ src/components/ProjectItem/ProjectItem.js
@@ -3,13 +3,21 @@ import "./ProjectItem.css";
 
 const ProjectItem = ({imgSrc, title, desc, github, link, skills}) => {
   return (
-    <div>
-      <p>imgSrc: {imgSrc}</p>
-      <p>title: {title}</p>
-      <p>desc: {desc}</p>
-      <p>github: {github}</p>
-      <p>link: {link}</p>
-      <p>skills: {skills}</p>
+    <div className="projectItem-container">
+      <div className="projectItem-img-desc">
+        <img src={imgSrc} alt={title} />
+        <p className="projectItem-desc">desc: {desc}</p>
+      </div>
+      {/* <picture>
+        <source media="(min-width: 650px)" srcSet={imgSrc} />
+        <img src={imgSrc} alt={title} />
+      </picture> */}
+      <div className="projectItem-brief">
+        <p><h5 className="projectItem-brief-title">{title}</h5></p>
+        <p>github: {github}</p>
+        <p>link: {link}</p>
+        <p>skills: {skills}</p>
+      </div>
     </div>
   );
   
