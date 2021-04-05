39006901fc24a4879f9c9d6014d1f7ec24873765
Weiyi Zhu <zhuwy99@gmail.com>
2021-04-04 22:54:29 -0700

    set up footer layout

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,27 @@
 import React from "react";
+import "./Footer.css";
 
 const Footer = (props) => {
   return (
-    <div id='contact'>
-      <h3>Footer</h3>
+    <div id="footer">
+      <div className="footer_container">
+        <h3>Weiyi Zhu</h3>
+        <div className="footer_info">
+          <p>Sammamish, WA</p>
+          <p>
+            Email:{" "}
+            <a href="mailto:wzhu1@swarthmore.edu">wzhu1@swarthmore.edu</a>
+          </p>
+          <p>
+            Phone: <a href="tel:+19495262914">(949) 526-2914</a>
+          </p>
+        </div>
+        <ul className="footer_socials">
+          <li>Email</li>
+          <li>Github</li>
+          <li>Linkedin</li>
+        </ul>
+      </div>
     </div>
   );
 };
