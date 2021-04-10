30d9fdcab9656844b9eb4fd5d3d7dcb545650625
Weiyi Zhu <zhuwy99@gmail.com>
2021-04-09 18:47:16 -0700

    finish about me layout

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,13 +6,34 @@ const About = () => {
   return (
     <section id="about">
       <img src={AboutMe} alt="About Me jpg"></img>
-      <p>
+      <div className="about-text-container">
         <h1>About Me</h1>
         <hr></hr>
-        I'm eager to secure a full-time developer position at 2021 that will
-        allow me to utilize my problem solving skills and attention to detail to
-        further develop my abilities in the field of computer science.
-      </p>
+        <div className="about-desc">
+          <p className="about-desc-bold">
+            I'm eager to secure a full-time developer position in 2021 that will
+            allow me to utilize my problem solving skills and attention to
+            detail to further develop my abilities in the field of computer
+            science.
+          </p>
+          <ul className="about-desc-bulletpt">
+            <li>
+              <span>Programming Languages:</span> Python, C#, JavaScript, C++,
+              Java.
+            </li>
+            <li>
+              <span>Frameworks/Tools:</span> React JS, Express JS, HTML, CSS,
+              jQuery, SQL, NoSQL, .NET, Git, AWS, agile.
+            </li>
+            <li>
+              <span>Hobbies:</span> NBA, Music, Tennis, Go (Chess).
+            </li>
+            <li>
+              <span>Languages:</span> English, Mandarin.
+            </li>
+          </ul>
+        </div>
+      </div>
     </section>
   );
 };
