d2afcf8073215c0ed52ba2b7ba86d80dd0fb2227
Weiyi Zhu <zhuwy99@gmail.com>
2021-04-11 10:39:24 -0700

    download background img to local

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,17 +1,16 @@
 import React from "react";
 import "./Home.css";
 import headshot from "../../static_files/Zhu.jpg";
+import background from "../../static_files/Background.jpeg";
 import Resume from "../../static_files/Resume.pdf";
 
 const Home = (props) => {
-  var image =
-    "https://images.pexels.com/photos/1631677/pexels-photo-1631677.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
   return (
     <>
       <div className="container">
         <img
           id="background_img"
-          src={image}
+          src={background}
           alt="background image"
         ></img>
         <div className="inner-container">
