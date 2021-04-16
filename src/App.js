262a65dac53bb503044a46bf5dc673c269607371
Weiyi Zhu <zhuwy99@gmail.com>
2021-04-15 18:20:11 -0700

    mobile layout

--- src/App.js
+++ src/App.js
@@ -2,11 +2,24 @@ import React from "react";
 import "./App.css";
 import NavBar from "./components/NavBar/NavBar";
 import Home from "./components/Home/Home";
-import Projects from "./components/Projects/Projects"
-import Footer from "./components/Footer/Footer"
+import Projects from "./components/Projects/Projects";
+import Footer from "./components/Footer/Footer";
 import About from "./components/About/About";
 
 function App() {
+  // const [windowWidth, setwindowWidth] = useState(window.innerWidth)
+
+  // const handleResize = () => {
+  //   setwindowWidth(window.innerWidth)
+  // }
+
+  // useEffect(() => {
+  //   window.addEventListener('resize', handleResize)
+
+  //   return () => {
+  //     window.removeEventListener('resize', handleResize)
+  //   }
+  // }, [])
   return (
     <div>
       <NavBar />
--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -1,20 +1,24 @@
 import React, { useState } from "react";
 import "./NavBar.css";
 import Resume from "../../static_files/Resume.pdf";
+import MenuIcon from "@material-ui/icons/Menu";
+import CloseIcon from "@material-ui/icons/Close";
 
 const NavBar = (props) => {
-  // const [clicked, setClicked] = useState(false);
+  const [clicked, setClicked] = useState(false);
 
-  // const handleClick = () => {
-  //   setClicked(!clicked);
-  // };
-  //https://www.prwhite.io/blog/sticky-navbar-hides-scroll
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
   return (
     <nav>
       <a className="logo" href="#">
         Weiyi Zhu
       </a>
-      <ul>
+      <div className="hamburger" onClick={handleClick}>
+        {clicked ? <CloseIcon /> : <MenuIcon />}
+      </div>
+      <ul className={clicked ? "nav-list-mobile" : "nav-list"}>
         <li>
           <a href="#about">About</a>
         </li>
@@ -25,7 +29,7 @@ const NavBar = (props) => {
           <a href="#footer">Contact</a>
         </li>
         <li>
-          <a href={Resume} download="Resume.pdf" className="download-btn2">
+          <a href={Resume} download="Resume.pdf">
             Resume
           </a>
         </li>
