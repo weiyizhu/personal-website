b25de6ded87ea2af5a468506def46dbba8d9f5f1
Weiyi <zhuwy99@gmail.com>
2021-09-02 10:21:49 -0400

    add news compare project

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,12 +3,47 @@ import ProjectItem from "../ProjectItem/ProjectItem";
 import "./Projects.css";
 import NinePM from "../../static_files/9PM.png";
 import PersonalWebsite from "../../static_files/PersonalWebsite.PNG";
+import NewsCompare from "../../static_files/News_Compare.png";
 
 const Project = (props) => {
   return (
     <section id="projects">
       <h1>Projects</h1>
       <hr></hr>
+      <ul className="projects_container">
+        <div className="projects_row">
+          <li>
+            <ProjectItem
+              imgSrc={NewsCompare}
+              title={"News Compare"}
+              brief_desc={
+                "A website that provides news comparisons across different news sources."
+              }
+              desc={[
+                "Customizable keywords searching, source selections, and filtering options.",
+                "Implemented user authentication with access and reset tokens.",
+                "Allowed users to save, manage and recover their favorite topics and news.",
+              ]}
+              github={"https://github.com/weiyizhu/News_Compare"}
+              link={"https://news-compare.com"}
+              skills={
+                "MongoDB, Express, React, Node, TypeScript, Redux, Material UI, JWT, React router dom, Heroku"
+              }
+            />
+          </li>
+          <li>
+            <ProjectItem
+              imgSrc={PersonalWebsite}
+              title={"Personal Website"}
+              brief_desc={"Personal Project - This Website ^_^"}
+              desc={["A website about me!"]}
+              github={"https://github.com/weiyizhu/personal_website"}
+              link={""}
+              skills={"React JS, CSS"}
+            />
+          </li>
+        </div>
+      </ul>
       <ul className="projects_container">
         <div className="projects_row">
           <li>
@@ -29,17 +64,7 @@ information.`,
               }
             />
           </li>
-          <li>
-            <ProjectItem
-              imgSrc={PersonalWebsite}
-              title={"Personal Website"}
-              brief_desc={"Personal Project - This Website ^_^"}
-              desc={["A website about me!"]}
-              github={"https://github.com/weiyizhu/personal_website"}
-              link={"leetcode link"}
-              skills={"React JS, CSS"}
-            />
-          </li>
+          <li></li>
         </div>
       </ul>
     </section>
